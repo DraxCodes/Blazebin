3fd8abedeeda3e8293e616e4157c2ad18a31155b
Draxcodes <joelparkinson@live.co.uk>
2022-10-04 20:40:47 +0100

    Move to node adapter

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	optimizeDeps: {
-		include: ['highlight.js', 'highlight.js/lib/core']
+		include: ['highlight.js', 'highlight.js/lib/core', '@vscode/vscode-languagedetection']
+	},
+	server: {
+		port: 5173,
+		strictPort: false
+	},
+	preview: {
+		port: 4173,
+		strictPort: false
 	}
 };
 
