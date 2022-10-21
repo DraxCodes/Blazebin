90888ac7acd22a475a3b13a518ab0d3bce5de513
Draxcodes <joelparkinson@live.co.uk>
2022-10-21 15:47:52 +0100

    Add default language for when auto fails

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,9 +19,14 @@
 			});
 
 			const langResult = await langresponse.json();
-			console.log(langResult);
+			console.log('lang result', langResult);
 
-			pasteLanguage.set(langResult[0].languageId);
+			if (langResult.length > 0) {
+				pasteLanguage.set(langResult[0].languageId);
+			} else {
+				pasteLanguage.set('md');
+			}
+
 			const id = nanoid(10);
 
 			const dbAddResponse = await fetch('/api/db/add', {
@@ -45,7 +50,6 @@
 </script>
 
 <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-	<!-- Replace with your content -->
 	<div class="px-4 py-6 sm:px-0">
 		<div class="flex items-start space-x-4">
 			<div class="min-w-0 flex-1">
